c60855ddc7de9a87d66f05e4f2cfd56a483e8add
lovers2020 <dkrkskswl@naver.com>
2023-12-27 14:54:00 +0900

    Create models

---
+++ reviews/models.py
@@ -0,0 +1,26 @@
+from tkinter import CASCADE
+from django.db import models
+from common.models import CommonModel
+
+
+class Review(CommonModel):
+    "Review from a user to a Room or Experience"
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+    )
+    room = models.ForeignKey(
+        "rooms.Room", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    payload = models.TextField()
+    rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user} / ⭐{self.rating}"
---
+++ users/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class languageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrentyChoices(models.TextChoices):
+        WON = "won", "Korean Won"
+        USD = "usd", "Dollar"
+
+    first_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    name = models.CharField(
+        max_length=150,
+        default="",
+    )
+    avatar = models.ImageField(blank=True)
+    is_host = models.BooleanField(
+        default=False,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=languageChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrentyChoices.choices,
+    )
