a979e68ebf1d5bc4e391a70af2f88e54bebf4cfd
lovers2020 <dkrkskswl@naver.com>
2024-01-29 23:41:29 +0900

    Update file

--- categories/models.py
+++ categories/models.py
@@ -7,11 +7,14 @@ class Category(CommonModel):
     """Room or Experience Category"""
 
     class CategoryKindChoices(models.TextChoices):
-        ROOMS = ("rooms", "Rooms")
-        EXPERIENCES = ("experiences", "Experiences")
+        ROOMS = "rooms", "Rooms"
+        EXPERIENCES = "experiences", "Experiences"
 
     name = models.CharField(max_length=50)
-    kind = models.CharField(max_length=15, choices=CategoryKindChoices)
+    kind = models.CharField(
+        max_length=15,
+        choices=CategoryKindChoices.choices,
+    )
 
     def __str__(self):
         return f"{self.kind.title()} : {self.name}"
